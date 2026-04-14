1112addc676e4ef3748b100596dcc8818981ae9f
Subhankar Prusty <subhankar99p@gmail.com>
2026-04-14 23:18:18 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -58,7 +58,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.FRONTEND_URL, "http://localhost:5173", "http://localhost:3000"],
+    allow_origins=[
+    "https://yatrika-delta.vercel.app",
+    "http://localhost:5173",
+    "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
